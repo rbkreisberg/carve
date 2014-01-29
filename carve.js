dbbb00cc3d02d6965dbb5c33cc37d216cea1c28b
Dick Kreisberg <rkreisberg@systemsbiology.org>
2014-01-28 17:56:04 -0800

    update semver and use multiple module system logic

--- carve.js
+++ carve.js
@@ -9,10 +9,22 @@ https://github.com/syntagmatic/parallel-coordinates
 Copyright (c) 2012, Kai Chang
 */
 
-(function() {
-  if (typeof module === "undefined") self.carve = carve;
-  else module.exports = carve;
-  carve.version = "0.0.5";
+(function (root, factory) {
+   if (typeof exports === 'object' && root.require) {
+     module.exports = factory(require("underscore"), require("d3"));
+   } else if (typeof define === "function" && define.amd) {
+      // AMD. Register as an anonymous module.
+      define(["underscore","d3"], function(_, d3) {
+        // Use global variables if the locals are undefined.
+        return factory(_ || root._, d3 || root.d3);
+      });
+   } else {
+      // RequireJS isn't being used. Assume underscore and d3 are loaded in <script> tags
+      factory(_, d3);
+   }
+}(this, function(_, d3) {
+
+  carve.version = "0.0.7";
 
 function carve(config) {
 
@@ -1590,4 +1602,6 @@ science.stats.kde = function() {
   return kde;
 };
 
-})();
+return carve;
+
+}));
